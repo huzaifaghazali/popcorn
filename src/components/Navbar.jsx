a7ece193fe96db691dd0c6eeb4c8648439a09534
huzaifaghazali <huzaifaghazali@gmail.com>
2023-09-12 17:55:49 +0500

    Fixed typo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Search from './Search';
 import Logo from './Logo';
+import NumbResults from './NumbResults';
 
 export default function Navbar() {
   return (
     <nav className='nav-bar'>
       <Logo />
       <Search />
+      <NumbResults />
     </nav>
   );
 }
---
+++ src/components/NumbResults.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function NumbResults() {
+  return (
+    <p className='num-results'>
+      Found <strong>X</strong> results
+    </p>
+  );
+}
