7e3af64e850725f7b58ae7f932a456f542e9ca2d
huzaifaghazali <huzaifaghazali@gmail.com>
2023-10-15 00:32:44 +0500

    Added prop types

--- src/components/StarRating.jsx
+++ src/components/StarRating.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import PropTypes from "prop-types";
 
 const containerStyle = {
   display: 'flex',
@@ -10,6 +11,16 @@ const starContainerStyle = {
   display: 'flex',
   // gap: '4px',
 };
+
+StarRating.propTypes = {
+ maxRating: PropTypes.number,
+ defaultRating: PropTypes.number,
+ color: PropTypes.string,
+ size: PropTypes.number,
+ messages: PropTypes.array,
+ className: PropTypes.string,
+ onSetRating: PropTypes.func
+}
 
 export default function StarRating({
   maxRating = 5,
@@ -36,7 +47,7 @@ export default function StarRating({
   };
 
   return (
-    <div style={containerStyle}>
+    <div style={containerStyle}  className={className}>
       <div style={starContainerStyle}>
         {Array.from({ length: maxRating }, (_, i) => (
           <Star
