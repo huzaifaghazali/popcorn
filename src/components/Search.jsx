7380f02c80665bc35ada207e0632ba791c9ca705
huzaifaghazali <huzaifaghazali@gmail.com>
2023-11-06 17:56:31 +0500

    Added input focus

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,6 +1,21 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 
 function Search({ query, setQuery }) {
+  const inputEl = useRef(null);
+
+  useEffect(() => {
+    function callback(e) {
+      if (document.activeElement === inputEl.current) return;
+      if (e.code === 'Enter') {
+        inputEl.current.focus();
+        setQuery('');
+      }
+    }
+
+    document.addEventListener('keydown', callback);
+
+    return () => document.addEventListener('keydown', callback);
+  }, [setQuery]);
 
   return (
     <input
@@ -9,6 +24,7 @@ function Search({ query, setQuery }) {
       placeholder='Search movies...'
       value={query}
       onChange={(e) => setQuery(e.target.value)}
+      ref={inputEl}
     />
   );
 }
